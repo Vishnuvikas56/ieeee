aba64e03fef1ee0c6dc943f45cc7c69964967544
Vishnuvikas56 <127959219+Vishnuvikas56@users.noreply.github.com>
2023-10-04 21:46:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,3 @@
-// const add = document.getElementById('add');
-// const del = document.getElementById('delete');
-// const noTasks = document.getElementById('TasksToday');
-// const tasks = document.getElementsByClassName('taskMade');
-// const textPH = document.getElementById('placeholderText');
-// const taskDes = document.getElementById('taskDes');
-// const taskDate = document.getElementById('date');
-// const priority = document.getElementById('priority');
-// const addTaskact = document.getElementById('addTaskact');
-// const editTasks = document.getElementById('editTasks');
-// const AllTasks = document.getElementById('tasks');
-// const inputTask = document.getElementById('inputTask');
-// const taskTime = document.getElementById('time');
-// const taskLabel = document.getElementsByClassName('taskLabel');
-// const taskDTP = document.getElementsByClassName('taskDTP');
-// var taskCount = 0;
-// var index = 0;
-// const taskForm = document.getElementById('taskForm');
-// function addTask(){
-//     for (let i = 0; i < taskCount; i++) {
-//         tasks[i].style.display = "block";
-//     }
-// }
-// addTaskact.addEventListener("click",()=>{
-//     editTasks.style.display = "flex";
-//     noTasks.style.display = "block";
-//     inputTask.style.display = "none";
-//     addTask();
-// })
-// add.addEventListener("click", () => {
-//     textPH.style.display = "none";
-//     editTasks.style.display = "none";
-//     noTasks.style.display = "none";
-//     inputTask.style.display = "flex";
-//     if(taskCount<5){
-//         taskCount++;
-//     }
-//     index++;
-// });
-// del.addEventListener("click", () => {
-    
-// });
-// taskForm.addEventListener('submit', function (e) {
-//     e.preventDefault(); 
-//     const x1 = taskDes.value;
-//     const x2 = taskDate.value;
-//     const x3 = taskTime.value;
-//     const x4 = priority.value;
-//     content["task"+(index)]["tasklabel"] = x1;
-//     content["task"+(index)]["taskdtp"] = "On " + x2 + " At " + x3 + " and priority is " + x4;
-//     document.getElementById("dtask"+(index)).innerHTML = content["task"+(index)]["tasklabel"];
-//     document.getElementById("ltask"+(index)).innerHTML = content["task"+(index)]["taskdtp"];
-// });
 var content = {
     "task1" : {
         "tasklabel" : "",
@@ -94,14 +41,12 @@ const taskDTP = document.getElementsByClassName('taskDTP');
 var taskCount = 0;
 var index = 0;
 
-// Function to add a new task
 function addTask() {
     for (let i = 0; i < taskCount; i++) {
         tasks[i].style.display = "block";
     }
 }
 
-// Event listener for the "Add Task" button
 addTaskact.addEventListener("click", () => {
     editTasks.style.display = "flex";
     noTasks.style.display = "block";
@@ -109,7 +54,6 @@ addTaskact.addEventListener("click", () => {
     addTask();
 });
 
-// Event listener for the "Add" button
 add.addEventListener("click", () => {
     textPH.style.display = "none";
     editTasks.style.display = "none";
@@ -121,23 +65,18 @@ add.addEventListener("click", () => {
     index++;
 });
 
-// Event listener for the "Delete" button
 del.addEventListener("click", () => {
     if (index > 0) {
-        // Remove the last task
         const lastTaskIndex = "task" + index;
         content[lastTaskIndex]["tasklabel"] = "";
         content[lastTaskIndex]["taskdtp"] = "";
         document.getElementById("dtask" + index).innerHTML = "";
         document.getElementById("ltask" + index).innerHTML = "";
-
-        // Update task count and index
         taskCount--;
         index--;
     }
 });
 
-// Event listener for the task form submission
 taskForm.addEventListener('submit', function (e) {
     e.preventDefault();
     const x1 = taskDes.value;
@@ -151,9 +90,6 @@ taskForm.addEventListener('submit', function (e) {
     document.getElementById("ltask" + index).innerHTML = content[currentTaskIndex]["taskdtp"];
 });
 
-// Initialize the content object
-
-// Initialize content object with up to 6 tasks
 for (let i = 1; i <= 6; i++) {
     content["task" + i] = {
         "tasklabel": "",
